96ad2ef245bf85eaeba3da7e42a0d703a2de6feb
jennaly <106183040+jennaly@users.noreply.github.com>
2022-11-30 21:29:34 -0800

    add layout and organize components in _app.js

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
     return (
-        <div>
-            Layout
+        <div className="layout">
+            <Head>
+                <title>JS Mastery Store</title>
+            </Head>
+
+            <header>
+                <Navbar />
+            </header>
+
+            <main className="main-container">
+                {children}
+            </main>
+
+            <footer>
+                <Footer />
+            </footer>
         </div>
     )
 }
