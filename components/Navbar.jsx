4779f6ee4628b9d62b6c526607237ca93c6b4c4a
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-07 12:08:57 -0800

    change store name

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     return (
         <div className="navbar-container">
             <p className="logo">
-                <Link href="/">JSM Headphones</Link>
+                <Link href="/">JL Headphones</Link>
             </p>
 
             <button type="button"
