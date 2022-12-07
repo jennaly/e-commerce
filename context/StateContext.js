4ac90bea5c3d8f6829c63aa36fef7b48cb606b67
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-06 16:20:30 -0800

    edit setCartItems operation in Cart so that product order is maintained throughout in-cart quantity changes

--- context/StateContext.js
+++ context/StateContext.js
@@ -58,21 +58,48 @@ export const StateContext = ({ children }) => {
     const toggleCartItemQuantity = (id, value) => {
         foundProduct = cartItems.find((item) => item._id === id);
         index = cartItems.findIndex((product) => product._id === id);
-        const newCartItems = cartItems.filter((item) => item._id !== id);
+        // const newCartItems = cartItems.filter((item) => item._id !== id);
 
-    
+        const incCartItemQty = cartItems.map(item => {
+            if (item._id === id) {
+                return {
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            } else {
+                return {
+                    ...item
+                }
+            }
+        })
+
+        const decCartItemQty = cartItems.map(item => {
+            if (item._id === id) {
+                return {
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+            } else {
+                return {
+                    ...item
+                }
+            }
+        })
+
         if(value === 'inc') {
-          setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
-          setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
-          setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+            setCartItems(incCartItemQty);
+            // setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
         } else if(value === 'dec') {
-          if (foundProduct.quantity > 1) {
-            setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
-            setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
-            setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
-          }
+            if (foundProduct.quantity > 1) {
+                setCartItems(decCartItemQty);
+                // setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
+                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+                setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+            }
         }
-      }
+    }
 
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
